d6120f0d01831b94b620ce119695e4c2b1f43400
CarlosAtencia <124048104+CarlosAtencia@users.noreply.github.com>
2024-12-09 11:36:42 +0100

    Add files via upload

--- animeInfo/view/assets/scripts/validarInput.js
+++ animeInfo/view/assets/scripts/validarInput.js
@@ -1,9 +1,9 @@
 // Expresiones regulares para validación
 
 const nombreUsuarioRegex = /[A-z0-9]{5,20}/; // Entre 5 y 20 caracteres alfanumericos
-const correoRegex = /^(?=.{6,30}$)[\w]+@[a-z]+\.[a-zA-Z]{2,}$/; // Correo que acabe en .com o .es y tenga entre 6 y 30 caracteres
+const correoRegex = /^(?=.{6,30}$)[\w]+@[a-z]+\.[a-zA-Z]{2,}$/; // Correo que tenga entre 6 y 30 caracteres
 const passwdRegex = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,20}/; // Contraseña entre 8 y 20 caracteres que tenga al menos una mayúscula, una minúscula y un número
-const comentarioRegex = /(?=\S)^([A-z¿?¡!\-_0-9\s]){10,400}(?<=\S)$/ // Comentario entre 10 y 400 carácteres con alfanumericos, simbolos y que no empiece ni acabe con espacios
+const comentarioRegex = /^(?=\S)([A-z¿?¡!\-_0-9\s]){10,400}(?<=\S)$/ // Comentario entre 10 y 400 carácteres con alfanumericos, simbolos y que no empiece ni acabe con espacios
 const listasRegex = /^(?=.*[A-z])[A-z0-9]{4,10}$/ // Lista debe tener entre 4 y 10 caracteres alfanuméricos con al menos una letra
 
 // Función para validar los inputs con expresiones regulares
